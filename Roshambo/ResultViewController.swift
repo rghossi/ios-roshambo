948c06a0e828a175404c79e7540f99e69986d360
rghossi <rafael.ghossi@gmail.com>
2017-03-25 11:16:36 -0300

    added second view controller to display result

---
+++ Roshambo/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  Roshambo
+//
+//  Created by Rafael Rodrigues Ghossi on 3/25/17.
+//  Copyright © 2017 Rafael Rodrigues Ghossi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
+
